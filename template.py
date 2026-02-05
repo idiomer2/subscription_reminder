17f1e10031bacbbd2b1c0e1d0fdffe2bef28b1e8
tt <tt@aliyun.hz>
2026-02-05 11:46:21 +0800

    update: add Bark and change cfg to ENV

--- template.py
+++ template.py
@@ -1,23 +1,24 @@
 """ TODO: 功能
-@crontab: 30 09 * * * cd ${BASE_PATH} && python -m folder.xxx 2>&1 | tee -a logs/xxx.log
+@crontab: 30 09 * * 1-5 cd $reminder_home && python -m folder.xxx 2>&1 | tee -a logs/xxx.log
 """
 
 from dotenv import dotenv_values
-from pyutils.notify_util import Feishu, Pushme
-from pyutils.date_util import now
+from pyutils.notify_util import Feishu, Pushme, Bark
+from pyutils.date_util import now, now_time
 
 
 
 if __name__ == '__main__':
-    cfg = dotenv_values()
+    ENV = dotenv_values()
     print(f'\n\n\n=============== {now()} ===============')
 
     # TODO
     title, content = 'title', 'test...'
 
     try:
-        Feishu(cfg['FEISHU_WEBHOOK_TOKEN']).send_markdown(title, content)
+        Feishu(ENV['FEISHU_WEBHOOK_TOKEN']).send_markdown(title, content)
     finally:
         cate, icon = '', '😀'
-        Pushme(cfg['PUSHME_PUSH_KEY']).send_markdown(f'[#{cate}!{icon}]'+title, content)
+        Pushme(ENV['PUSHME_PUSH_KEY']).send_markdown(f'[#{cate}!{icon}]'+title, content)
+        Bark(ENV['BARK_TOKEN']).send(content, title)
 
